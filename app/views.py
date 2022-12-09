0204e933032d6fb3aa5ca0019f6042a5700a7990
Lanrayy <lanre.sodeinde1@gmail.com>
2022-12-09 19:51:51 +0000

    Basic API completed

--- app/views.py
+++ app/views.py
@@ -1,4 +1,27 @@
 from app import app
+from flask_restful import Api, Resource
+import json
+
+api = Api(app)
+
+
+
+class AddressBook(Resource):
+    def get(self):
+        pass
+
+    def post(self):
+        pass
+    
+    def put(self):
+        pass
+
+    def delete(self):
+        pass
+
+
+    
+api.add_resource(AddressBook, "/", "/<genre>", "/<genre>/<new_genre>")
 
 @app.route('/')
 def index():
