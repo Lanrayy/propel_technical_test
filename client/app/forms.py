856b39b3ec7e85e51c6bd62e0a3d5e8d45e2d6d3
Lanrayy <lanre.sodeinde1@gmail.com>
2022-12-13 23:34:21 +0000

    Updated Add Contact Form Definition

--- client/app/forms.py
+++ client/app/forms.py
@@ -1,7 +1,9 @@
-from flask_wtf import Form
-from wtforms import StringField
-from wtforms import IntegerField
+from flask_wtf import FlaskForm
+from wtforms import StringField, EmailField
 from wtforms.validators import InputRequired
 
-class GenreForm(Form):
-    genre = StringField('genre', validators=[InputRequired()])
+class AddContactForm(FlaskForm):
+    first_name = StringField('first_name', validators=[InputRequired()])
+    last_name = StringField('last_name', validators=[InputRequired()])
+    phone = StringField('phone', validators=[InputRequired()])
+    email = EmailField('email', validators=[InputRequired()])
