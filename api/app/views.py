15ef018509fecec4524cce9e4dfdc8063a9f167d
Lanrayy <lanre.sodeinde1@gmail.com>
2022-12-14 00:02:12 +0000

    JSON flat file added

--- api/app/views.py
+++ api/app/views.py
@@ -1,18 +1,11 @@
 from app import app
-from flask import jsonify
+from flask import jsonify, request
 from flask_restful import Api, Resource
 import json
 
 api = Api(app)
 
-# filename="./book.json"
-
-# function to add to the JSON flat file
-def write_json(data, filename="book.json"):
-    with open(filename, "w") as file:
-        json.dump(data, file, indent=4)
-
-
+# Address book class
 class AddressBook(Resource):
     def get(self):
         with open("app/book.json", "r+") as file:
@@ -20,30 +13,31 @@ class AddressBook(Resource):
             file_data = json.load(file)
             print(file_data)
 
-            for record in file_data:
-                print(record)
-
-            # details = [{"firstname":"Jim"}]
-            print("Check")
-            print(file_data[0])
             # return a json with all the file details
             return jsonify(file_data)
 
-    def post(self, new_details): 
+    def post(self):
         with open("app/book.json", "r+") as file:
+            print(request.data)
+            # convert request bytes into json
+            data = json.loads(request.data.decode('utf-8'))
+
+            print(data)
+            print(data.get('first_name'))
+
             # read the content of the file
             file_data = json.load(file)
 
             # add new details to the
-            file_data.append(new_details)
+            file_data.append(data)
 
-            # return a json with all the file details
+            # return a json with all the file details and update JSON flat file
             file.seek(0)
             json.dump(file_data, file, indent = 4)
 
-            return "Added to file"
+            # return success message
+            return jsonify({"Success" : "Contact Added"})
 
-        pass
     
     def put(self, new_details, old_details):
         with open("app/book.json", "r+") as file:
@@ -53,19 +47,38 @@ class AddressBook(Resource):
             for record in file_data:
                 print(record)
 
-            # file_data[old_details]
-
-            # return a json with all the file details
             file.seek(0)
             json.dump(file_data, file, indent = 4)
 
-            return "Added to file"
-        pass
+            return jsonify({"Success" : "Contact Updated"})
 
     def delete(self):
-        pass
+        print(request.data)
+        data = json.loads(request.data.decode('utf-8'))
+        update = []
 
+        with open("app/book.json", "r") as file:
+            # read the content of the file
+            file_data = json.load(file)
 
-    
-# api.add_resource(AddressBook, "/", "/<new_details>", "/<new_details>/<old_details>")
+            # find the contact, skip over it
+            # append the rest to a new array
+            for contact in file_data:
+                print(contact)
+                if contact['first_name'] == data.get('first_name') and contact['last_name'] == data.get('last_name') and contact['email'] == data.get('email') :
+                    print("MATCH found")
+                    pass
+                else:
+                    update.append(contact)
+                
+        print("**************New info*******************")
+        print(update)
+
+        # Update the JSON flat file
+        with open("app/book.json", "w") as file:
+            json.dump(update, file, indent=4)
+        
+        return jsonify({"Success" : "Contact Deleted"})
+            
+            
 api.add_resource(AddressBook, "/")
